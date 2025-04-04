73783ec4e9450113a0bbefe6587869e43025f0f8
Fabricio Daniel Turrina <fabriciodanielturrina@gmail.com>
2025-04-04 12:55:23 -0300

    changes in  vite.config.js

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,11 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-
-
   css: {
     preprocessorOptions: {
       css: {
@@ -13,5 +11,13 @@ export default defineConfig({
       },
     },
   },
-
+  server: {
+    proxy: {
+      "/api": {
+        target: "https://fipe-zqv6.onrender.com",
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
 });
